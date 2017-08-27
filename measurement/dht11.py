e5f9f510db7a2c4fa1b1b6c0b92f3d633b3c6de4
vpagal <pagalosb@gmail.com>
2017-08-27 13:32:24 +0300

    Create first package for DHT11 sensor (temperature and humidity)

---
+++ measurement/dht11.py
@@ -0,0 +1,28 @@
+###############################################################
+# Author : Vasilis Pagalos (vpagal)
+# email  : pagalosb@gmail.com
+# Desc   : package for DHT-11 temperature and humidity sensor
+###############################################################
+
+import RPi.GPIO as GPIO
+
+class DHT11:
+	_error_code  = ''
+	_temperature = -256
+	_humidity    = 0
+ 
+	def __init__(self, GPIONpin):
+		# Constructor of DHT11 - set data pin
+		GPIO.setmode(GPIO.BCM)
+		GPIO.setup(GPIOpin, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+	def _getData():
+		# Private method for get sensor data
+
+	def getTemperature(self):
+		# Getter for temperature
+		return self._temperature
+
+	def getHumidity(self):
+		# Getter for humidity
+		return self._humidity
